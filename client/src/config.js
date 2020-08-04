e3152b3aa1562315b774a369bad243c8bd62baaf
Catherine <cath@catherines-mbp.home>
2020-08-04 15:56:53 -0500

    Fixed file paths, added scripts - prep for Heroku

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,5 @@
-export const imageUrl = process.env.REACT_APP_IMAGE_URL || 'http://localhost:5000';
+export const imageUrl = process.env.REACT_APP_IMAGE_URL ||
+    process.env.NODE_ENV === 'production'
+    ? 'https ://shinyday.herokuapp.com/'
+    : 'http://localhost:5000';
 export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
