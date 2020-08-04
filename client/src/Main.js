2024f08caa9df27e3e573fa787f1d776fb238771
Catherine <cath@catherines-mbp.home>
2020-08-04 16:15:06 -0500

    Added antd package and adjusted start scripts

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Divider } from 'antd';
 
 import { baseUrl } from './config';
 
@@ -29,6 +30,7 @@ export default ({ token }) => {
         <>
             <Header token={token} username={username}/>
             <Banner />
+            <Divider />
         </>
     );
 };
