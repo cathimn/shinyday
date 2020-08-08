56260eddae3a2b7033ea47d4188fb97979560635
cathimn <catclee@gmail.com>
2020-08-07 22:00:43 -0500

    Clean up music api

--- routes/api/music.js
+++ routes/api/music.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { Op } = require('sequelize')
+const AWS = require('aws-sdk');
+
+const { accessKeyId, secretAccessKey, region } = require('../.././config/index');
+
+AWS.config.update({
+    region,
+    accessKeyId,
+    secretAccessKey,
+});
+
+const s3 = new AWS.S3();
 
 const router = express.Router();
 
 const { Artist, Album, Song } = require('../../db/models')
 
-// api/music/latest returns 5 latest albums
-// TODO not sure if orders by newest createdDate?
+const regex = /[\s|\W]/gm;
+
 router.get('/latest', asyncHandler(async (req, res) => {
     const flat = new Array();
     const latest = await Album.findAll({
@@ -26,21 +37,25 @@ router.get('/latest', asyncHandler(async (req, res) => {
     res.json(flat)
 }));
 
-// find one artist GET
-// api/music/:artistId or :artistName
-router.get('/:artistId', asyncHandler(async (req, res) => {
-    const { artistId } = req.params;
-    const artist = await Artist.findOne({
-        where: {
-            id: artistId,
-        }
+router.get('/:artistTerm', asyncHandler(async (req, res) => {
+    const { artistTerm } = req.params;
+    const artists = await Artist.findAll({
+        attributes: ["id", "artist_name"]
     });
-
-
+    const matches = artists.filter(ele => {
+        let name = ele.artist_name;
+        name = name.toLowerCase();
+        name = name.replace(regex, "");
+        return (name === artistTerm) ? name : null;
+    })
+    console.log(matches)
+    if (matches.length > 0) {
+        res.json(matches);
+    } else {
+        res.json(false);
+    }
 }));
 
-// api/music/tracklist RETURNS THE TRACKLIST OF AN ALBUM BY ID
-// TODO can i make this find by albumname too?
 router.post('/tracklist', asyncHandler(async (req, res) => {
     const { searchId } = req.body;
     const tracklist = await Album.findOne({
@@ -52,10 +67,54 @@ router.post('/tracklist', asyncHandler(async (req, res) => {
     res.json(tracklist.songs.map(ele => ele.name))
 }));
 
-// api/music/discography RETURNS THE DISCOGRAPHY OF AN ARTIST
-// search by ID? name? TODO
-router.post('/discography', asyncHandler(async (req, res) => {
+router.post('/songfiles', asyncHandler(async (req, res) => {
+    const { prefix } = req.body;
+    const fullPrefix = `artists/${prefix}`
+    const response = await s3.listObjectsV2({
+        Bucket: 'shinyday',
+        Prefix: fullPrefix,
+        Delimiter: ".mp3"
+    }).promise();
+    const songUrls = response.CommonPrefixes.map(ele => ele.Prefix)
+    res.json(songUrls);
+}))
 
+router.post('/songnames', asyncHandler(async (req, res) => {
+    const { album } = req.body;
+    let albumId = null;
+
+    const albums = await Album.findAll({
+        attributes: ["id", "name"]
+    });
+
+    for (let i = 0; i < albums.length; i++) {
+        if ((albums[i].name).toLowerCase().replace(regex, "").includes(album)) {
+            albumId = albums[i].id;
+            break;
+        }
+    }
+
+    const songs = await Album.findAll({
+        include: [{ model: Song, as: "songs", required: true, attributes: ["track_num", "name"]}],
+        where: {
+            id: albumId,
+        }
+    })
+
+    res.json(songs[0].songs);
+}))
+
+router.get('/discography/:id', asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const albums = await Album.findAll({
+        attributes: ["name"],
+        where: {
+            artist_id: id,
+        }
+    })
+    const array = albums.map(ele => ele.name)
+
+    res.json(array);
 }));
 
 module.exports = router;
