49775b8b26fb78e82080f3d176fbe156dde54ef4
cathimn <catclee@gmail.com>
2020-08-07 22:25:11 -0500

    Changing bucket url

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@ export const imageUrl = process.env.REACT_APP_IMAGE_URL ||
     ? 'https://shinyday.herokuapp.com'
     : 'http://localhost:5000';
 export const baseUrl = process.env.REACT_APP_IMAGE_URLL || `${imageUrl}/api`;
-export const bucketUrl = 'https://shinyday.s3.us-east-2.amazonaws.com';
+export const bucketUrl = 'http://shinyday.s3.us-east-2.amazonaws.com';
