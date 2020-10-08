953689511f08868bc8cbb3469d9a9912291f65bf
cathimn <catclee@gmail.com>
2020-10-07 19:30:25 -0500

    Change Express helmet

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const routes = require('./routes/index');
 const app = express();
 
 app.use(cors({ origin: true }));
-app.use(helmet({ hsts: false }));
+app.use(helmet());
+// app.use(helmet({ hsts: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,25 +20,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.use(function(_req, _res, next) {
-    next(createError(404));
+  next(createError(404));
 })
 
 app.use(function (err, _req, res, _next) {
-    res.status(err.status || 500);
-    if (err.status === 401) {
-        res.set('WWW-Authenticate', 'Bearer');
-    }
-    res.json({
-        message: err.message,
-        error: JSON.parse(JSON.stringify(err)),
-    });
+  res.status(err.status || 500);
+  if (err.status === 401) {
+      res.set('WWW-Authenticate', 'Bearer');
+  }
+  res.json({
+      message: err.message,
+      error: JSON.parse(JSON.stringify(err)),
+  });
 });
 
 module.exports = app;
